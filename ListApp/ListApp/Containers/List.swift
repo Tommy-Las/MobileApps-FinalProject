2aa7e5f9db9c3561e9eb22f0861f00690ae18c56
agalvezv <43121059+agalvezv@users.noreply.github.com>
2022-11-15 13:00:05 -0500

    Adding Classes
    Container folder represents objects.

--- ListApp/ListApp/Containers/List.swift
+++ ListApp/ListApp/Containers/List.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+
+class List {
+    
+    var name: String
+    var id: Int
+    //var listinfo: ListInfo
+    
+    
+    init(name: String, id: Int)
+    {
+        self.name = name
+        self.id = id
+        //self.listinfo = ListInfo()
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
