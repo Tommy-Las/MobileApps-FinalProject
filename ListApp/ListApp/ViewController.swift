a0c546b627b201c7bcc3871a57b8d660bb6f6bb7
agalvezv <43121059+agalvezv@users.noreply.github.com>
2022-11-15 11:33:19 -0500

    Project Upload
    Project insertion into repo

---
+++ ListApp/ListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Alexandro Galvez-Vega on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
